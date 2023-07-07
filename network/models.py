6ac3b9056b7ef066ba1aeb912a81ae2e3c40e0c9
OscarCarde <oscrdns@gmail.com>
2023-07-07 20:17:48 +0100

    posts summary in models + optional media

--- network/models.py
+++ network/models.py
@@ -7,7 +7,10 @@ class User(AbstractUser):
 
 class Post(models.Model):
     content = models.CharField(max_length = 1000)
-    media = models.ImageField(upload_to='media/post_media')
+    media = models.ImageField(upload_to='media/post_media', blank=True)
 
     by = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
     timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.by.username} said: {self.content[:9]}... on {self.timestamp.strftime('%d-%m-%Y')} at {self.timestamp.strftime('%H:%M')}"
